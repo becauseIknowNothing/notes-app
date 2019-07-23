277ea74b8d2a5f255256597e9f62739cadfca50c
unknown <lovish.madhu.2404@gmail.com>
2019-07-23 13:11:14 +0530

    Finished notes-app

---
+++ app.js
@@ -0,0 +1,64 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const methods = require('./notes')
+
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        methods.addNote(argv.title, argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        methods.removeNote(argv.title)
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        methods.listNotes()
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        methods.readNote(argv.title)
+    }
+})
+
+yargs.parse()
